58ecea7e8c19397930d78c3ef39e02302e62537f
slo64 <slowpoke64@gmail.com>
2018-04-04 02:42:57 +0300

    Descriptions, cleanup

--- Libraries/custom_lib.py
+++ Libraries/custom_lib.py
@@ -1,12 +1,3 @@
-# 5. Используя эту библиотеку, написать автотесты на Robot Framework,
-# реализующие как минимум:
-# a. Проверку аутентификации при различных user, password
-# (используя data driven подход, реализуемый при помощи Template)
-# b. Проверку какого-либо header в возвращаемом ответе /get
-# c. Проверку количества строк в возвращаемом запросе /stream
-# d. Проверка кода возврата во всех ответах
-# 6. Все проверки должны быть в коде автотестов, а не в библиотеке
-
 import requests
 from urllib.parse import urljoin
 
@@ -16,49 +7,31 @@ class custom_lib(object):
     def __init__(self):
         self._result = ''
 
+    # Basic auth for httpbin.org
     def custom_basic_auth(self, url, user, password):
         self._result = requests.get(url, auth=(user, password))
         return self._result.status_code
 
+    # Get request for httpbin.org
     def custom_get(self, url):
         self._result = requests.get(url)
 
+    # Stream request for httpbin.org
     def custom_stream(self, url, n):
         self._result = requests.get(url.strip('/') + '/' + str(n))
 
-    # a. Проверку аутентификации при различных user, password
-    # (используя data driven подход, реализуемый при помощи
-    # Template)
-    def check_authenticated(self, b):
-        try:
-            auth = self._result.json()['authenticated']
-            if auth != b:
-                raise AssertionError(("Authentication error:"
-                                      "Authenticated: %s, "
-                                      "Expected %s") % (auth, b))
-        except:
-            raise AssertionError(self._result.content)
+    # Authentication check for the result
+    def get_authenticated(self):
+        return self._result.json()['authenticated']
 
-    # b. Проверку какого-либо header в возвращаемом ответе /get
+    # Get headers from the get response (not the request headers)
     def get_headers(self):
         return self._result.json()['headers']
-        # if received != string:
-        #     raise AssertionError(("Wrong header %s:"
-        #                           "Received %s, "
-        #                           "Expected %s") % (key, received, expected))
 
-    # c. Проверку количества строк в возвращаемом запросе /stream
-    def check_lines(self, n):
-        l = len(list(self._result.iter_lines()))
-        if l != n:
-            raise AssertionError(("Wrong number of lines:"
-                                  "Received %d, "
-                                  "Expected %d") % (l, n))
+    # Get number of lines in the response
+    def get_lines(self):
+        return len(list(self._result.iter_lines()))
 
-    # d. Проверка кода возврата во всех ответах
-    def check_status(self, n):
-        if self._result.status_code != n:
-            raise AssertionError(("Wrong status code:"
-                                  " %d, Expected %d") %
-                                 (self._result.status_code, n))
-
+    # Get status code from the response
+    def get_status(self):
+        return self._result.status_code
